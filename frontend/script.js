4f1969e3f78a97b72e20728b1a97b25146083aa1
Benno Dierickx <91146451+DierickxBenno@users.noreply.github.com>
2022-10-25 20:47:27 +0200

    add:  basic design and fetch code

--- frontend/script.js
+++ frontend/script.js
@@ -2,17 +2,26 @@
 
 let htmlAddButton, htmlWave, htmlPercentage;
 
-
-const updateView = function (progressPercentage) {
-
+const fetchFromAPI = function (url) {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "x-api-key":
+        "b80bf5de9fc0c4b3b5d76b38251fb5c88318bfc700b77bbb294ddd4871246e9e",
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
-const listenToSocket = function () {
-
-};
-
-const listenToUI = function () {
-
-}
-
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  let lat = "50.8194776";
+  let lng = "3.2577263";
+  //   let url = `https://api.ambeedata.com/latest/pollen/by-lat-lng?lat=${lat}&lng=${lng}`;
+  fetchFromAPI(url);
+});
