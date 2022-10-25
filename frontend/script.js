55899f88dc8a9b1da267ee63266d1a7ce8740b03
Benno Dierickx <91146451+DierickxBenno@users.noreply.github.com>
2022-10-25 17:07:38 +0200

    start

---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let htmlAddButton, htmlWave, htmlPercentage;
+
+
+const updateView = function (progressPercentage) {
+
+};
+
+const listenToSocket = function () {
+
+};
+
+const listenToUI = function () {
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {});
